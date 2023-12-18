2dcc510ca25a1c55364da107f2735056ae4891eb
Mohamed Sharaf <23546820+mosharafMS@users.noreply.github.com>
2023-12-18 16:56:44 -0500

    update sql

--- Allfiles/Labs/01/setup.ps1
+++ Allfiles/Labs/01/setup.ps1
@@ -1,41 +1,42 @@
 Clear-Host
 write-host "Starting script at $(Get-Date)"
+
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name Az.Synapse -Force
 
 # Handle cases where the user has multiple subscriptions
 $subs = Get-AzSubscription | Select-Object
 if($subs.GetType().IsArray -and $subs.length -gt 1){
-    Write-Host "You have multiple Azure subscriptions - please select the one you want to use:"
-    for($i = 0; $i -lt $subs.length; $i++)
-    {
-            Write-Host "[$($i)]: $($subs[$i].Name) (ID = $($subs[$i].Id))"
-    }
-    $selectedIndex = -1
-    $selectedValidIndex = 0
-    while ($selectedValidIndex -ne 1)
-    {
-            $enteredValue = Read-Host("Enter 0 to $($subs.Length - 1)")
-            if (-not ([string]::IsNullOrEmpty($enteredValue)))
-            {
-                if ([int]$enteredValue -in (0..$($subs.Length - 1)))
+        Write-Host "You have multiple Azure subscriptions - please select the one you want to use:"
+        for($i = 0; $i -lt $subs.length; $i++)
+        {
+                Write-Host "[$($i)]: $($subs[$i].Name) (ID = $($subs[$i].Id))"
+        }
+        $selectedIndex = -1
+        $selectedValidIndex = 0
+        while ($selectedValidIndex -ne 1)
+        {
+                $enteredValue = Read-Host("Enter 0 to $($subs.Length - 1)")
+                if (-not ([string]::IsNullOrEmpty($enteredValue)))
                 {
-                    $selectedIndex = [int]$enteredValue
-                    $selectedValidIndex = 1
+                    if ([int]$enteredValue -in (0..$($subs.Length - 1)))
+                    {
+                        $selectedIndex = [int]$enteredValue
+                        $selectedValidIndex = 1
+                    }
+                    else
+                    {
+                        Write-Output "Please enter a valid subscription number."
+                    }
                 }
                 else
                 {
                     Write-Output "Please enter a valid subscription number."
                 }
-            }
-            else
-            {
-                Write-Output "Please enter a valid subscription number."
-            }
-    }
-    $selectedSub = $subs[$selectedIndex].Id
-    Select-AzSubscription -SubscriptionId $selectedSub
-    az account set --subscription $selectedSub
+        }
+        $selectedSub = $subs[$selectedIndex].Id
+        Select-AzSubscription -SubscriptionId $selectedSub
+        az account set --subscription $selectedSub
 }
 
 # Prompt user for a password for the SQL Database
@@ -78,7 +79,7 @@ foreach ($provider in $provider_list){
 # Generate unique random suffix
 [string]$suffix =  -join ((48..57) + (97..122) | Get-Random -Count 7 | % {[char]$_})
 Write-Host "Your randomly-generated suffix for Azure resources is $suffix"
-$resourceGroupName = "dp000-$suffix"
+$resourceGroupName = "dp500-$suffix"
 
 # Choose a random region
 Write-Host "Finding an available region. This may take several minutes...";
@@ -132,9 +133,7 @@ New-AzResourceGroup -Name $resourceGroupName -Location $Region | Out-Null
 # Create Synapse workspace
 $synapseWorkspace = "synapse$suffix"
 $dataLakeAccountName = "datalake$suffix"
-$sparkPool = "spark$suffix"
 $sqlDatabaseName = "sql$suffix"
-$adxpool = "adx$suffix"
 
 write-host "Creating $synapseWorkspace Synapse Analytics workspace in $resourceGroupName resource group..."
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
@@ -142,17 +141,11 @@ New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
   -Mode Complete `
   -workspaceName $synapseWorkspace `
   -dataLakeAccountName $dataLakeAccountName `
-  -sparkPoolName $sparkPool `
+  -uniqueSuffix $suffix `
   -sqlDatabaseName $sqlDatabaseName `
   -sqlUser $sqlUser `
   -sqlPassword $sqlPassword `
-  -adxPoolName $adxpool `
-  -uniqueSuffix $suffix `
   -Force
-
-# Pause Data Explorer pool
-write-host "Pausing the $adxpool Data Explorer Pool..."
-Stop-AzSynapseKustoPool -Name $adxpool -ResourceGroupName $resourceGroupName -WorkspaceName $synapseWorkspace -NoWait
 
 # Make the current user and the Synapse service principal owners of the data lake blob store
 write-host "Granting permissions on the $dataLakeAccountName storage account..."
@@ -162,7 +155,6 @@ $userName = ((az ad signed-in-user show) | ConvertFrom-JSON).UserPrincipalName
 $id = (Get-AzADServicePrincipal -DisplayName $synapseWorkspace).id
 New-AzRoleAssignment -Objectid $id -RoleDefinitionName "Storage Blob Data Owner" -Scope "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$dataLakeAccountName" -ErrorAction SilentlyContinue;
 New-AzRoleAssignment -SignInName $userName -RoleDefinitionName "Storage Blob Data Owner" -Scope "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$dataLakeAccountName" -ErrorAction SilentlyContinue;
-
 
 # Create database
 write-host "Creating the $sqlDatabaseName database..."
@@ -182,19 +174,9 @@ Get-ChildItem "./data/*.txt" -File | Foreach-Object {
 write-host "Pausing the $sqlDatabaseName SQL Pool..."
 Suspend-AzSynapseSqlPool -WorkspaceName $synapseWorkspace -Name $sqlDatabaseName -AsJob
 
-# Upload files
-write-host "Loading data..."
-$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $dataLakeAccountName
-$storageContext = $storageAccount.Context
-Get-ChildItem "./files/*.csv" -File | Foreach-Object {
-    write-host ""
-    $file = $_.Name
-    Write-Host $file
-    $blobPath = "sales_data/$file"
-    Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
-}
-
-# Create KQL script
-New-AzSynapseKqlScript -WorkspaceName $synapseWorkspace -DefinitionFile "./files/ingest-data.kql"
+# Upload solution script
+write-host "Uploading script..."
+$solutionScriptPath = "Solution.sql"
+Set-AzSynapseSqlScript -WorkspaceName $synapseWorkspace -DefinitionFile $solutionScriptPath -sqlPoolName $sqlDatabaseName -sqlDatabaseName $sqlDatabaseName
 
 write-host "Script completed at $(Get-Date)"
